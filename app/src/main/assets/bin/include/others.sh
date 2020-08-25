b615e3be6fe70f4a611adc78cc1e6b86d4a2c69b
Flytreels <3021182972@qq.com>
2020-08-25 09:35:25 +0800

    fix

--- app/src/main/assets/bin/include/others.sh
+++ app/src/main/assets/bin/include/others.sh
@@ -16,18 +16,11 @@ enable_proot_fakekernel(){
 if [ -e "$CONFIG_DIR/fake_kernel" ];then
 rm $CONFIG_DIR/fake_kernel
 rm $rootfs/proc/.version
-
-
 else
 echo "$kernel" > $CONFIG_DIR/fake_kernel
 cat <<- EOF > "$rootfs/proc/.version"
 Linux version $kernel (dogeland@fakehost) (gcc version 4.9.x 20150123 (prerelease) (GCC) ) #1 SMP PREEMPT Fri Jul 10 00:00:00 UTC 2020
 EOF
-fi
-
-else
-rm $TOOLKIT/fake_kernel
-rm $rootfs/proc/.version
 fi
 }
 
