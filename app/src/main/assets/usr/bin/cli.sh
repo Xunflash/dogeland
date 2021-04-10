6a2273354a2b6a677a4adb8cce59cc486d11ba0e
Flytreels <3021182972@qq.com>
2021-04-10 09:30:08 +0800

    Update : App NEW released 1.5.0-dev2

--- app/src/main/assets/usr/bin/cli.sh
+++ app/src/main/assets/usr/bin/cli.sh
@@ -4,7 +4,7 @@
 # 
 # license: GPL-v3.0
 #
-VERSION=2.2.6_DEBUG
+VERSION=3.0.1_DEBUG
 if [ -d "/dogeland/" ];then
   TOOLKIT=/dogeland/
   else
@@ -16,16 +16,26 @@ else
   echo "">/dev/null
 fi
 . $TOOLKIT/include/others.sh
-. $TOOLKIT/include/stop_rootfs.sh & . $TOOLKIT/include/del_rootfs.sh
-. $TOOLKIT/include/platform.sh & . $TOOLKIT/include/set_env.sh
-. $TOOLKIT/include/check_rootfs.sh & . $TOOLKIT/include/del_rootfs.sh
-. $TOOLKIT/include/backup_rootfs.sh & . $TOOLKIT/include/deploy_linux.sh
-. $TOOLKIT/include/vkfs_init.sh & . $TOOLKIT/include/starter_proot.sh
-. $TOOLKIT/include/starter_unshare.sh & . $TOOLKIT/include/starter_auto.sh
-. $TOOLKIT/include/exec_unshare.sh & . $TOOLKIT/include/exec_auto.sh
-. $TOOLKIT/include/exec_local-shell.sh & . $TOOLKIT/include/extra_dropbear.sh
-. $TOOLKIT/include/extra_sshd.sh & . $TOOLKIT/include/extra_vncserver.sh
-. $TOOLKIT/include/extra_patcher.sh & . $TOOLKIT/include/exec_proot.sh
+. $TOOLKIT/include/stop_rootfs.sh
+. $TOOLKIT/include/del_rootfs.sh
+. $TOOLKIT/include/platform.sh
+. $TOOLKIT/include/set_env.sh
+. $TOOLKIT/include/check_rootfs.sh
+. $TOOLKIT/include/del_rootfs.sh
+. $TOOLKIT/include/backup_rootfs.sh
+. $TOOLKIT/include/deploy_linux.sh
+. $TOOLKIT/include/vkfs_init.sh
+. $TOOLKIT/include/starter_proot.sh
+. $TOOLKIT/include/starter_unshare.sh
+. $TOOLKIT/include/starter_auto.sh
+. $TOOLKIT/include/exec_unshare.sh
+. $TOOLKIT/include/exec_auto.sh
+. $TOOLKIT/include/exec_local-shell.sh
+. $TOOLKIT/include/extra_dropbear.sh
+. $TOOLKIT/include/extra_sshd.sh
+. $TOOLKIT/include/extra_vncserver.sh
+. $TOOLKIT/include/extra_patcher.sh
+. $TOOLKIT/include/exec_proot.sh
 if [ ! -n "${1}" ]; then
   echo "Tools Start Successfully"
 fi
