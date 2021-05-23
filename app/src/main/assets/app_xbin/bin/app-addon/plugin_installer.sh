03687fd3da08457d7d1f5e5d334b78df549c16ed
Your Name <you@example.com>
2021-05-23 12:49:28 +0800

    Refactor: App

--- app/src/main/assets/app_xbin/bin/app-addon/plugin_installer.sh
+++ app/src/main/assets/app_xbin/bin/app-addon/plugin_installer.sh
@@ -4,6 +4,6 @@ unzip $file -d $TMPDIR/cache_pkg
 chmod -R 0770 $TMPDIR/cache_pkg
 echo "- Installing"
 . $TMPDIR/cache_pkg/info.config.sh
-cp -R $FILE $TOOLKIT/bin
+cp -R $FILE $TOOLKIT/bin/
 rm -rf $TMPDIR/cache_pkg
 echo "- All done"
